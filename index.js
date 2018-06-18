e02c8fb51f090daed483607156e4a5e5d392dd9b
Lukasz Bondarewicz <lukasz@parcelvision.com>
2018-06-18 11:58:18 +0100

    bump to 0.0.26

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var express = require('express');
+var version = require('./package.json').version;
 
 var DEFAULT_PORT = 8080;
 var PORT = process.env.PORT || DEFAULT_PORT;
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('Hello gcloud-pipeline 0.0.25\n');
+  res.send(`Hello gcloud-pipeline ${version}`);
 });
 
-app.listen(PORT)
+app.listen(PORT);
 console.log('Running on http://localhost:' + PORT);
