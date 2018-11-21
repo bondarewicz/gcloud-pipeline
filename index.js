f7fa8a00d8cdc488d391ae10eae4e07dd9682a04
Łukasz Bondarewicz <bondarewicz@gmail.com>
2018-11-21 16:40:49 +0000

    zzz-001 (#35)

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = process.env.PORT || DEFAULT_PORT;
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send(`gcloud-pipeline xyz-123 v.${version}`);
+  res.send(`gcloud-pipeline zzz-001 v.${version}`);
 });
 
 app.listen(PORT);
